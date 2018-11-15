0ca53c378688b347ee4601de4e9af785ff57b279
Kevin Jhonson <kevinjh07@outlook.com>
2018-11-15 14:07:50 -0200

    New app icon, title, package name, removed not used icons, android link mode: SdkOnly.

--- Cryptopia.Public/Cryptopia.Public.Android/MainActivity.cs
+++ Cryptopia.Public/Cryptopia.Public.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Prism;
 using Prism.Ioc;
 
 namespace Cryptopia.Public.Droid {
-    [Activity(Label = "Cryptopia API", Icon = "@mipmap/ic_cryptopia", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Cryptopia Public API", Icon = "@mipmap/ic_cryptopia", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
         protected override void OnCreate(Bundle bundle) {
             TabLayoutResource = Resource.Layout.Tabbar;
